b89de3659dd81b1d04062d5c903f92b2b58118cd
mi2254 <90284025+mi2254@users.noreply.github.com>
2021-09-23 03:36:16 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -82,7 +82,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
    clientSocket.send(quitCommand.encode())
    recv1 = clientSocket.recv(1024).decode()
    print(recv1)
-  
+   clientSocket.close()
 
    # Fill in end
 
